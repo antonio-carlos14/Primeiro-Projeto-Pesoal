d6108fab244184f1d31c4cd7831c9a7db999b77a
Antônio Carlos <antcarfelix1357@gmail.com>
2026-09-15 14:17:26 -0300

    Add study schedule based on the day of the week

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-
+//Calendário de estudos//
+const dia="dias da semana"
+//coloque o dia a semana em que você está os objetivos//
+if(dia==="segunda") {
+                console.log("matemática financeira (destina-se 4 horas)")
+} else if(dia==="terça"){
+                console.log("aprofundamento sobre geometrias (destina-se 4 horas)")
+} else if(dia==="quarta") {
+        console.log("revisar/finalizar conteúdos vistos em sala (destina-se 4 horas)")
+} else if(dia==="quinta") {
+        console.log("física (destina-se 2 horas)")
+} else if (dia==="sexta") {
+        console.log("linguagens (destina-se 2 horas)")
+} else if(dia==="sabado") {
+        console.log("estudo livre (destina-se 1:30)")
+} else {
+        console.log("hoje não é um dia útil destinado à tarefas")
+}
