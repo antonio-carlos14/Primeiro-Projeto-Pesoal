cc2f630bcab9bd5eb29b19f2ab3dae9fb40bd2fd
Antônio Carlos <antcarfelix1357@gmail.com>
2026-09-22 19:24:11 -0300

    Update verification logic for December studies

--- Primeiro-Projeto-Pesoal-main (2)/index.js
+++ Primeiro-Projeto-Pesoal-main (2)/index.js
@@ -189,7 +189,7 @@ if (diaAtual === 15 && dezembro_15 === true) {
 }
 
 
-// DIA 15
+// VERACIDADE E CHECAGEM DE ESTUDOS
 if ( dezembro_1 === true &&dezembro_2 === true &&dezembro_3 === true &&dezembro_4 === true &&dezembro_5 === true &&dezembro_6 === true && dezembro_7 === true && dezembro_8 === true && dezembro_9 === true &&
     dezembro_10 === true && dezembro_11 === true &&dezembro_12 === true &&dezembro_13 === true &&dezembro_14 === true && dezembro_15 === true) {
     console.log("Parabens! conteúdos de revisão do 9 ano concluídos com sucesso")
