972b48326e819ebbfd383a38890c64124cfc4cb6
Antônio Carlos <antcarfelix1357@gmail.com>
2026-09-22 16:47:01 -0300

    Refactor study calendar for December

--- Primeiro-Projeto-Pesoal-main (2)/index.js
+++ Primeiro-Projeto-Pesoal-main (2)/index.js
@@ -1,135 +1,238 @@
-//Calendário de estudos mensal (DEZEMBRO & JANEIRO)//
-let dezembro_1=true
-let dezembro_2=true
-let dezembro_3=true
-let dezembro_4=true
-let dezembro_5=true
-let dezembro_6=true
-let dezembro_7=true
-let dezembro_8=true
-let dezembro_9=true
-let dezembro_10=true
-let dezembro_11=true
-let dezembro_12=true
-let dezembro_13=true
-let dezembro_14=true
-let dezembro_15=true
+ // Calendário de estudos mensal (DEZEMBRO) //
+let dezembro_1 = true
+let dezembro_2 = true
+let dezembro_3 = true
+let dezembro_4 = true
+let dezembro_5 = true
+let dezembro_6 = true
+let dezembro_7 = true
+let dezembro_8 = true
+let dezembro_9 = true
+let dezembro_10 = true
+let dezembro_11 = true
+let dezembro_12 = true
+let dezembro_13 = true
+let dezembro_14 = true
+let dezembro_15 = true
 
+// ================================================================================= //
+// DEFINA O DIA EM QUE VOCÊ DESEJA CONSULTAR //
+let diaAtual = 15
+// ================================================================================= //
 
-//=========================================================================================================//
-//DEFINA O DIA EM QUE VOCÊ DEEJA CONSULTAR//
-let diaAtual = 1
-//coloque o dia do mês em que você está,os objetivos e as horas destinadas//
-//as condicionais abaixo irão checar o cronograma destinado de acordo com cada dia do mês//
-//condicionais referentes ao mês de dezembro//
+// 1. EXIBIÇÃO DA TAREFA DO DIA SELECIONADO //
 
-if(diaAtual === 1)
-    if(dezembro_1=== true) {
-                console.log("Matemática financeira (destina-se 4 horas)")
-} else {
-                console.log("Atividade não realizada")
-}
-
-if(diaAtual === 2)
-    if(dezembro_2=== true){
-                console.log("Aprofundamento sobre geometrias (destina-se 4 horas)")
-} else {
-                console.log("Atividade não realizada")
-} 
-
-if(diaAtual === 3)
-    if(dezembro_3=== true) {
-        console.log("Revisar/finalizar conteúdos vistos em sala (destina-se 4 horas)")
-} else {
-        console.log("Atividade não realizada")
-}
-
-if(diaAtual === 4)
-    if(dezembro_4=== true) {
-        console.log("física (destina-se 2 horas)")
-} else {
-    console.log("Atividade não realizada")
-}
-   
-if(diaAtual === 5)
-    if (dezembro_5=== true) {
-        console.log("linguagens (destina-se 2 horas)")
-} else {
-    console.log("Atividade não realizada")
-}
-    
-if(diaAtual === 6)
-    if(dezembro_6=== true) {
-        console.log("estudo livre (destina-se 1:30)")
-} else {
-    console.log("Atividade não realizada")
-}
-    
-if(diaAtual === 7)
-    if(dezembro_7=== true) {
-        console.log("Geometria espacial (destina-se 4 horas)")
-} else {
-        console.log("Atividade não realizada")
-}
-
-if(diaAtual === 8)
-    if(dezembro_8=== true) {
-    console.log("Revisar noções de cálculo (destina-se 3 horas)")
-} else {
-    console.log("Atividade realizada")
-}
-
-if(diaAtual === 9)
-    if(dezembro_9=== true) {
-    console.log("Estudar geometria analítica(destina-se 4 horas)")
-} else {
+// DIA 1
+if (diaAtual === 1 && dezembro_1 === true) {
+    console.log("Matemática financeira, destina-se 4 horas")
+} else if (diaAtual === 1 && dezembro_1 === false) {
     console.log("Atividade não realizada")
 }
 
-if(diaAtual === 10)
-    if(dezembro_10=== true) {
-    console.log("Estudar geometria plana (destina-se 3:30")
-} else {
-    console.log("Atividade não finalizada")
-}
-if(diaAtual === 11)
-    if(dezembro_11=== true) {
-    console.log("Estudar geometria não euclidiana (destina-se 4 horas)")
-} else {
-    console.log("Atividade não realizada")
-}
-if(diaAtual === 12)
-    if(dezembro_12=== true) {
-    console.log("Estudar geometria fractal (destina-se 2 horas)")
-} else {
+// DIA 2
+if (diaAtual === 2 && dezembro_2 === true) {
+    console.log("Aprofundamento sobre geometrias, destina-se 4 horas")
+} else if (diaAtual === 2 && dezembro_2 === false) {
     console.log("Atividade não realizada")
 }
 
-if(diaAtual === 13)
-    if(dezembro_13=== true) {
-    console.log("Estudar geometria descritiva (destina-se 3 horas)")
-} else {
-    console.log("Atividade não finalizada")
-}
-
-if(diaAtual === 14)
-    if(dezembro_14=== true) {
-    console.log("Estudar cálculo integral (destina-se 4 horas)")
-} else {
+// DIA 3
+if (diaAtual === 3 && dezembro_3 === true) {
+    console.log("Revisar/finalizar conteúdos vistos em sala, destina-se 4 horas")
+} else if (diaAtual === 3 && dezembro_3 === false) {
     console.log("Atividade não realizada")
 }
 
-if(diaAtual === 15)
-    if(dezembro_15=== true) {
-    console.log("Estudar cálculo vetorial (destina-se  2:30)")
-} else {
+// DIA 4
+if (diaAtual === 4 && dezembro_4 === true) {
+    console.log("Física, destina-se 2 horas")
+} else if (diaAtual === 4 && dezembro_4 === false) {
+    console.log("Atividade não realizada")
+}
+// DIA 5
+if (diaAtual === 5 && dezembro_5 === true) {
+    console.log("Linguagens, destina-se 2 horas")
+} else if (diaAtual === 5 && dezembro_5 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 6
+if (diaAtual === 6 && dezembro_6 === true) {
+    console.log("Estudo livre, destina-se 1:30")
+} else if (diaAtual === 6 && dezembro_6 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 7
+if (diaAtual === 7 && dezembro_7 === true) {
+    console.log("Geometria espacial, destina-se 4 horas")
+} else if (diaAtual === 7 && dezembro_7 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 8
+if (diaAtual === 8 && dezembro_8 === true) {
+    console.log("Revisar noções de cálculo, destina-se 3 horas")
+} else if (diaAtual === 8 && dezembro_8 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 9
+if (diaAtual === 9 && dezembro_9 === true) {
+    console.log("Estudar geometria analítica, destina-se 4 horas")
+} else if (diaAtual === 9 && dezembro_9 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 10
+if (diaAtual === 10 && dezembro_10 === true) {
+    console.log("Estudar geometria plana, destina-se 3:30")
+} else if (diaAtual === 10 && dezembro_10 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 11
+if (diaAtual === 11 && dezembro_11 === true) {
+    console.log("Estudar geometria não euclidiana, destina-se 4 horas")
+} else if (diaAtual === 11 && dezembro_11 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 12
+if (diaAtual === 12 && dezembro_12 === true) {
+    console.log("Estudar geometria fractal, destina-se 2 horas")
+} else if (diaAtual === 12 && dezembro_12 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 13
+if (diaAtual === 13 && dezembro_13 === true) {
+    console.log("Estudar geometria descritiva, destina-se 3 horas")
+} else if (diaAtual === 13 && dezembro_13 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 14
+if (diaAtual === 14 && dezembro_14 === true) {
+    console.log("Estudar cálculo integral, destina-se 4 horas")
+} else if (diaAtual === 14 && dezembro_14 === false) {
+    console.log("Atividade não realizada")
+}
+
+// DIA 15
+if (diaAtual === 15 && dezembro_15 === true) {
+    console.log("Estudar cálculo vetorial, destina-se 2:30")
+} else if (diaAtual === 15 && dezembro_15 === false) {
     console.log("Atividade não realizada")
 }
 
 
-//=============================================================================================================//
+// ================================================================================= //
+// 2. CÁLCULO FINAL PROGRESSIVO DO DIA SELECIONADO (DO DIA 1 AO 15)
+// ================================================================================= //
 
-//coloque o dia do mês em que você está,os objetivos e as horas destinadas//
-//as condicionais abaixo irão checar o cronograma destinado de acordo com cada dia do mês//
-//condicionais referentes ao mês de dezembro//
+// DIA 1
+if (diaAtual === 1 && dezembro_1 === true ) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 1 && dezembro_1 === false) {
+    console.log("Estudos de geometrias incompleto")
+}
 
+// DIA 2
+if (diaAtual === 2 && dezembro_2 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 2) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 3
+if (diaAtual === 3  && dezembro_3 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 3) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 4
+if (diaAtual === 4 && dezembro_4 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 4) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 5
+if (diaAtual === 5 && dezembro_5 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 5) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 6
+if (diaAtual === 6  && dezembro_6 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 6) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 7
+if (diaAtual === 7 && dezembro_7 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 7) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 8
+if (diaAtual === 8 && dezembro_8 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 8) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 9
+if (diaAtual === 9 && dezembro_9 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 9) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 10
+if (diaAtual === 10 && dezembro_10 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 10) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 11
+if (diaAtual === 11 && dezembro_11 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 11) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 12
+if (diaAtual === 12 && dezembro_12 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 12) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 13
+if (diaAtual === 13 && dezembro_13 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 13) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 14
+if (diaAtual === 14 && dezembro_14 === true) {
+    console.log("Parabens! conteúdo de geometrias finalizado")
+} else if (diaAtual === 14) {
+    console.log("Estudos de geometrias incompleto")
+}
+
+// DIA 15
+if ( dezembro_1 === true &&dezembro_2 === true &&dezembro_3 === true &&dezembro_4 === true &&dezembro_5 === true &&dezembro_6 === true && dezembro_7 === true && dezembro_8 === true && dezembro_9 === true &&
+    dezembro_10 === true && dezembro_11 === false &&dezembro_12 === true &&dezembro_13 === true &&dezembro_14 === true && dezembro_15 === true) {
+    console.log("Parabens! conteúdos de revisão do 9 ano concluídos com sucesso")
+} else if (diaAtual === 15) {
+    console.log("Estudos incompletos, mais atenção e responsabilidade no próximos afazeres escolares!")
+}
