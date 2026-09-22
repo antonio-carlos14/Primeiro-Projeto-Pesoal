ad0021b2280875f7db0b77d6669e59a83fc32252
Antônio Carlos <antcarfelix1357@gmail.com>
2026-09-22 14:38:26 -0300

    Update inDex.js

--- Primeiro-Projeto-Pesoal-main (2)/inDex.js
+++ Primeiro-Projeto-Pesoal-main (2)/inDex.js
@@ -1,6 +1,6 @@
 // Calendário de estudos mensal (DEZEMBRO)
 let dezembro_1 = true
-let dezembro_2 = true
+let dezembro_2 = false
 let dezembro_3 = true
 let dezembro_4 = true
 let dezembro_5 = true
@@ -20,68 +20,68 @@ let dezembro_15 = true
 let diaAtual = 2
 // ================================================================================= //
 
-// CONDICIONAIS DE DEZEMBRO (Usando 'else if' e o operador '&&')
+// CONDICIONAIS DE DEZEMBRO
 
-if (diaAtual === 1 && dezembro_1) {
-    console.log("Matemática financeira, destina-se 4 horas");
-} else if (diaAtual === 1 && !dezembro_1) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 2 && dezembro_2) {
-    console.log("Aprofundamento sobre geometrias, destina-se 4 horas");
-} else if (diaAtual === 2 && !dezembro_2) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 3 && dezembro_3) {
-    console.log("Revisar/finalizar conteúdos vistos em sala, destina-se 4 horas");
-} else if (diaAtual === 3 && !dezembro_3) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 4 && dezembro_4) {
-    console.log("Física, destina-se 2 horas");
-} else if (diaAtual === 4 && !dezembro_4) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 5 && dezembro_5) {
-    console.log("Linguagens, destina-se 2 horas");
-} else if (diaAtual === 5 && !dezembro_5) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 6 && dezembro_6) {
-    console.log("Estudo livre, destina-se 1:30");
-} else if (diaAtual === 6 && !dezembro_6) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 7 && dezembro_7) {
-    console.log("Geometria espacial, destina-se 4 horas");
-} else if (diaAtual === 7 && !dezembro_7) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 8 && dezembro_8) {
-    console.log("Revisar noções de cálculo, destina-se 3 horas");
-} else if (diaAtual === 8 && !dezembro_8) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 9 && dezembro_9) {
-    console.log("Estudar geometria analítica, destina-se 4 horas");
-} else if (diaAtual === 9 && !dezembro_9) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 10 && dezembro_10) {
-    console.log("Estudar geometria plana, destina-se 3:30");
-} else if (diaAtual === 10 && !dezembro_10) {
-    console.log("Atividade não finalizada");
-} else if (diaAtual === 11 && dezembro_11) {
-    console.log("Estudar geometria não euclidiana, destina-se 4 horas");
-} else if (diaAtual === 11 && !dezembro_11) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 12 && dezembro_12) {
-    console.log("Estudar geometria fractal, destina-se 2 horas");
-} else if (diaAtual === 12 && !dezembro_12) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 13 && dezembro_13) {
-    console.log("Estudar geometria descritiva, destina-se 3 horas");
-} else if (diaAtual === 13 && !dezembro_13) {
-    console.log("Atividade não finalizada");
-} else if (diaAtual === 14 && dezembro_14) {
-    console.log("Estudar cálculo integral, destina-se 4 horas");
-} else if (diaAtual === 14 && !dezembro_14) {
-    console.log("Atividade não realizada");
-} else if (diaAtual === 15 && dezembro_15) {
-    console.log("Estudar cálculo vetorial, destina-se 2:30");
-} else if (diaAtual === 15 && !dezembro_15) {
-    console.log("Atividade não realizada");
+if (diaAtual === 1 && dezembro_1 === true) {
+    console.log("Matemática financeira, destina-se 4 horas")
+} else if (diaAtual === 1 && dezembro_1 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 2 && dezembro_2 === true) {
+    console.log("Aprofundamento sobre geometrias, destina-se 4 horas")
+} else if (diaAtual === 2 && dezembro_2 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 3 && dezembro_3 === true) {
+    console.log("Revisar/finalizar conteúdos vistos em sala, destina-se 4 horas")
+} else if (diaAtual === 3 && dezembro_3 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 4 && dezembro_4 === true) {
+    console.log("Física, destina-se 2 horas")
+} else if (diaAtual === 4 && dezembro_4 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 5 && dezembro_5 === true) {
+    console.log("Linguagens, destina-se 2 horas")
+} else if (diaAtual === 5 && dezembro_5 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 6 && dezembro_6 === true) {
+    console.log("Estudo livre, destina-se 1:30")
+} else if (diaAtual === 6 && dezembro_6 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 7 && dezembro_7 === true) {
+    console.log("Geometria espacial, destina-se 4 horas")
+} else if (diaAtual === 7 && dezembro_7 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 8 && dezembro_8 === true) {
+    console.log("Revisar noções de cálculo, destina-se 3 horas")
+} else if (diaAtual === 8 && dezembro_8 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 9 && dezembro_9 === true) {
+    console.log("Estudar geometria analítica, destina-se 4 horas")
+} else if (diaAtual === 9 && dezembro_9 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 10 && dezembro_10 === true) {
+    console.log("Estudar geometria plana, destina-se 3:30")
+} else if (diaAtual === 10 && dezembro_10 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 11 && dezembro_11 === true) {
+    console.log("Estudar geometria não euclidiana, destina-se 4 horas")
+} else if (diaAtual === 11 && dezembro_11 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 12 && dezembro_12 === true) {
+    console.log("Estudar geometria fractal, destina-se 2 horas")
+} else if (diaAtual === 12 && dezembro_12 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 13 && dezembro_13 === true) {
+    console.log("Estudar geometria descritiva, destina-se 3 horas")
+} else if (diaAtual === 13 && dezembro_13 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 14 && dezembro_14 === true) {
+    console.log("Estudar cálculo integral, destina-se 4 horas")
+} else if (diaAtual === 14 && dezembro_14 === false) {
+    console.log("Atividade não realizada")
+} else if (diaAtual === 15 && dezembro_15 === true) {
+    console.log("Estudar cálculo vetorial, destina-se 2:30")
+} else if (diaAtual === 15 && dezembro_15 === false) {
+    console.log("Atividade não realizada")
 } else {
-    console.log("Dia não cadastrado no cronograma");
+    console.log("Dia não cadastrado no cronograma")
 }
